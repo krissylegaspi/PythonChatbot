1a5b6be42ff6234fabef784d29c0afaab6592df0
Kristine Legaspi <krissylegaspi@gmail.com>
2021-03-22 12:05:36 -0700

    first chatbot

---
+++ echo.py
@@ -0,0 +1,5 @@
+print("Welcome to Kristine's first chatbot!")
+print("In this project, the chatbot will repeat what is typed.")
+
+stuff_to_echo = input("Type something...\n")
+print("You said: " + stuff_to_echo)
